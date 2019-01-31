32fcb9b3decdeae18f5060592cbe08284640a2db
kp54 <kangpang65@gmail.com>
2019-02-01 06:05:19 +0900

    add "lstags.py"

---
+++ lstags.py
@@ -0,0 +1,24 @@
+# ファイルのタグを吐くツール
+
+import os.path
+import sys
+
+import taglib
+
+
+def main():
+    if len(sys.argv) < 1:
+        return -1
+    if not(os.path.exists(sys.argv[1]) and os.path.isfile(sys.argv[1])):
+        return -2
+
+    try:
+        f = taglib.File(sys.argv[1])
+        for k, v in f.tags.items():
+            print(f'{k}: {v}')
+    finally:
+        f.close()
+
+
+if __name__ == '__main__':
+    sys.exit(main())
