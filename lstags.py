3c612363bc87af97a8fca74b736b88dac2a94242
kp54 <kangpang65@gmail.com>
2019-02-02 10:18:09 +0900

    死んだときにメッセージを吐く

--- lstags.py
+++ lstags.py
@@ -8,13 +8,16 @@ import taglib
 
 def main():
     if len(sys.argv) < 1:
+        print('usage: lstag <file>')
         return -1
     if not(os.path.exists(sys.argv[1]) and os.path.isfile(sys.argv[1])):
+        print('File does not exist.')
         return -2
 
     try:
         f = taglib.File(sys.argv[1])
-    except OSError:
+    except OSError as e:
+        print(e)
         return -3
     else:
         try:
